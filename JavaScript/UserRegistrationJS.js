14684c6a9c60cbc6094e8a984029a156e90752ee
Mohanee <mohanee99@gmail.com>
2020-11-22 23:23:05 +0530

    [mohanee]Add css, js and html files to validate the user input

---
+++ JavaScript/UserRegistrationJS.js
@@ -0,0 +1,36 @@
+const salary=document.querySelector('#salary');
+const output=document.querySelector('.salary-output');
+output.textContent=salary.value;
+salary.addEventListener('input',function(){
+output.textContent=salary.value;
+})
+
+//UC1  Validate Name
+const text=document.querySelector('#text');
+const textError=document.querySelector('.text-error');
+text.addEventListener('input', function(){
+let namRegex= new RegExp(/^[A-Z][a-z]{2,}$/);
+if(namRegex.test(text.value))
+    textError.textContent="";
+else
+    textError.textContent="Name is incorrect";
+});
+ 
+//UC2  Validate Email
+const email = document.querySelector('#email');
+const emailError = document.querySelector('.email-error');
+email.addEventListener('input',() => emailError.textContent = 
+                        new RegExp('^[A-Za-z0-9]+([._+-][A-Za-z0-9]+)*[@][A-Za-z0-9]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2,3})?$').test(email) ?
+                        '' : 'Email is Incorrect');
+                        
+
+//UC3  Validate Phone Number
+const phone = document.querySelector('#number');
+const phoneError = document.querySelector('.number-error');
+phone.addEventListener('input',() => phoneError.textContent = new RegExp('^[7-9][0-9]{9}$').test(phone) ? '' : 'Phone Number is incorrect' );
+
+
+//UC4  Validate Password
+const password = document.querySelector('#pwd');
+const passwordError = document.querySelector('.password-error');
+password.addEventListener('input', () => passwordError.textContent = new RegExp('^(?=.*[A-Z])(?=.*[0-9])(?=[A-Za-z0-9]*[-~!@#$%^&*()_+=`,.<>?/][A-Za-z0-9]*$).{8,}$').test(password) ? '' : 'Password is Incorrect');
